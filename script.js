20c759cc6a51fa5b20362c7b4e3afc443b9c53d6
priyadarshini8932 <shreyapriyadarshini032@gmail.com>
2026-04-26 15:25:46 +0530

    updated code

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ let addPassword1=document.getElementById("box1");
 
 let addPassword2=document.getElementById("box2");
 
-let heading=document.getElementsByTagName("h1");
-console.log(heading)
+let lengthInput=document.getElementById("length");
 
-const characters =[
-    'A','B','C','D','E','F','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*','&','$','#','!','?','<','>','+'
-]
+let heading=document.getElementsByTagName("h1");
+
+let symbolToggle = document.getElementById("symbols-toggle");
+let letters = [
+    'A','B','C','D','E','F','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+let symbols = ['*','&','$','#','!','?','<','>','+'];
 function start_generate(){
     addPassword1.textContent=generate_password();
     addPassword2.textContent=generate_password();
 }
 function generate_password(){
     let password="";
-    for(let i=0;i<15;i++){
+    let length = lengthInput.value;
+    let characters = [...letters];
+    if(symbolToggle.checked){
+        characters=characters.concat(symbols);
+    }
+    for(let i=0;i<length;i++){
         let x=Math.floor(Math.random()*characters.length);
         password +=characters[x];
     }
@@ -31,4 +41,17 @@ generatePassword.addEventListener("click", () => {
 
 function changeHeadingColor(){
     heading[1].style.color="#ECFDF5";
+}
+
+addPassword1.addEventListener("click", () => {
+    copyText(addPassword1);
+});
+
+addPassword2.addEventListener("click",()=>{
+    copyText(addPassword1);
+})
+function copyText(element){
+    let text=element.textContent;
+    navigator.clipboard.writeText(text);
+    alert("Copied!");
 }
